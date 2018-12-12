9330c3d89ab982758537744229a77c5604476fca
StrikerJS <minestrikerpr0@gmail.com>
2018-12-12 22:11:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,26 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/client.html');
 });
 
+let usersConnected = [];
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  socket.on('user login', function (msg) {
+    usersConnected.push(msg.userInfo);
+    console.log('User joined ! Name:'+msg.userInfo.userName)
+    console.log("Users connected:"+usersConnected.length);
+    io.emit('scoreUpdateAll', usersConnected);
+  });
+  socket.on('scoreUpdate', function(msg) {
+    for (let i = 0;i < usersConnected;i++) {
+      let user = usersConnected[i]
+      if (user.userInfo.userName == msg.userName) {
+        user.score = msg.score;
+        io.emit('scoreUpdateAll', usersConnected);
+      }
+    }
+  })
 });
 
